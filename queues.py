8d85055925c2dffcbc365e3d1c695ac9de9e1949
Yli <salazarylizanicole@gmail.com>
2022-12-10 20:32:53 +0800

    error in Cell In [10], line 1

--- queues.py
+++ queues.py
@@ -1,20 +1,5 @@
-# queues.py
+from queues import Queue
 
-from collections import deque
-
-class Queue:
-    def __init__(self, *elements):
-        self._elements = deque(elements)
-
-    def __len__(self):
-        return len(self._elements)
-
-    def __iter__(self):
-        while len(self) > 0:
-            yield self.dequeue()
-
-    def enqueue(self, element):
-        self._elements.append(element)
-
+class Stack(Queue):
     def dequeue(self):
-        return self._elements.popleft()
+        return self._elements.pop()
