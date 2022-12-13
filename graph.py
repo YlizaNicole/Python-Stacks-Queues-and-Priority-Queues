2448182156a7dd2ca07cb8a09042d501fcdc0041
Yli <salazarylizanicole@gmail.com>
2022-12-14 05:34:36 +0800

    custom data type representing a city in your road map

---
+++ graph.py
@@ -0,0 +1,20 @@
+# graph.py
+
+from typing import NamedTuple
+
+class City(NamedTuple):
+    name: str
+    country: str
+    year: int | None
+    latitude: float
+    longitude: float
+
+    @classmethod
+    def from_dict(cls, attrs):
+        return cls(
+            name=attrs["xlabel"],
+            country=attrs["country"],
+            year=int(attrs["year"]) or None,
+            latitude=float(attrs["latitude"]),
+            longitude=float(attrs["longitude"]),
+        )
