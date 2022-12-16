f31758574f9c6d39b360a3c1e5c517d4ca187981
Yli <salazarylizanicole@gmail.com>
2022-12-16 13:03:58 +0800

    defining data type to represent a job

--- async_queues.py
+++ async_queues.py
@@ -5,8 +5,13 @@ import asyncio
 from collections import Counter
 from urllib.parse import urljoin
 from bs4 import BeautifulSoup
-
+import sys
+from typing import NamedTuple
 import aiohttp
+
+class Job(NamedTuple):
+    url: str
+    depth: int = 1
 
 async def main(args):
     session = aiohttp.ClientSession()
@@ -15,6 +20,22 @@ async def main(args):
         display(links)
     finally:
         await session.close()
+
+async def worker(worker_id, session, queue, links, max_depth):
+    print(f"[{worker_id} starting]", file=sys.stderr)
+    while True:
+        url, depth = await queue.get()
+        links[url] += 1
+        try:
+            if depth <= max_depth:
+                print(f"[{worker_id} {depth=} {url=}]", file=sys.stderr)
+                if html := await fetch_html(session, url):
+                    for link_url in parse_links(url, html):
+                        await queue.put(Job(link_url, depth + 1))
+        except aiohttp.ClientError:
+            print(f"[{worker_id} failed at {url=}]", file=sys.stderr)
+        finally:
+            queue.task_done()
 
 async def fetch_html(session, url):
     async with session.get(url) as response:
