1e0ad5d751e50fa1e54945ab5a9109e235eb6125
Yli <salazarylizanicole@gmail.com>
2022-12-16 13:35:38 +0800

    update MD5-reversing function

--- multiprocess_queue.py
+++ multiprocess_queue.py
@@ -22,10 +22,10 @@ class Combinations:
             ]
             for i in reversed(range(self.length))
         )
-        
+
 def reverse_md5(hash_value, alphabet=ascii_lowercase, max_length=6):
     for length in range(1, max_length + 1):
-        for combination in product(alphabet, repeat=length):
+        for combination in Combinations(alphabet, length):
             text_bytes = "".join(combination).encode("utf-8")
             hashed = md5(text_bytes).hexdigest()
             if hashed == hash_value:
