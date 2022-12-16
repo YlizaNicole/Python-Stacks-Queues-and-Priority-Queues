232369af7a869ecdb3f7c56f3fb6ff540f3191c7
Yli <salazarylizanicole@gmail.com>
2022-12-16 23:45:17 +0800

    implementing kfka3

--- producer.py
+++ producer.py
@@ -1,16 +1,11 @@
 # producer.py
 
-import pika
+from kafka3 import KafkaProducer
 
-QUEUE_NAME = "mailbox"
-
-with pika.BlockingConnection() as connection:
-    channel = connection.channel()
-    channel.queue_declare(queue=QUEUE_NAME)
-    while True:
-        message = input("Message: ")
-        channel.basic_publish(
-            exchange="",
-            routing_key=QUEUE_NAME,
-            body=message.encode("utf-8")
-        )
+producer = KafkaProducer(bootstrap_servers="localhost:9092")
+while True:
+    message = input("Message: ")
+    producer.send(
+        topic="datascience",
+        value=message.encode("utf-8"),
+    )
